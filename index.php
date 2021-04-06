d00d7ea0b62cad461a8f410d759be6b0a7f5fccf
dimapenkin <star26909@gmail.com>
2021-04-06 22:35:05 +0300

    lab5.1

--- index.php
+++ index.php
@@ -3,21 +3,14 @@
 ini_set("display_errors", 1);
 error_reporting (-1); 
 
-require "core/EquationInterface.php";
-require "core/LogAbstract.php";
-require "core/LogInterface.php";
-
-require "penkin/PenkinException.php";
-require "penkin/MyLog.php";
-require "penkin/LinearEquationSolver.php";
-require "penkin/QuadEquationSolver.php";
+require "vendor/autoload.php";
 
 use penkin\MyLog;
 
 $solver = new penkin\QuadEquationSolver();
 
 try {
-    MyLog::log("Program version " . trim(file_get_contents("version")));
+    MyLog::log("Program version " . trim(file_get_contents("version")) . "\n\r");
     echo "Enter 3 numbers: a, b, c.\n\r";
 
     $a = readline("Enter a: \n\r");
--- penkin/PenkinException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace penkin;
-
-class PenkinException extends \RuntimeException {
-    
-}
